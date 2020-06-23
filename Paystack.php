f41a57cc7b7cd29baedc71faffbf59bd7e86d76e
Kayode Omolaoye <31309781+kayusgold@users.noreply.github.com>
2020-06-23 10:24:48 +0100

    Update Paystack

--- Paystack.php
+++ Paystack.php
@@ -1,9 +1,4 @@
 <?php
-// ini_set('display_errors', 1);
-// ini_set('display_startup_errors', 1);
-// error_reporting(E_ALL);
-
-//include_once("/var/www/cellcore.com.ng/paycollect/payment/PaystackBaseclass.php");
 include_once("PaystackBaseclass.php");
 
 class Paystack extends PaystackBaseClass
